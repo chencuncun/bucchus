338cf9d1f35ee48cb1a1568f71568203c78ee08e
chencuncun <chencuncun@chencuncun-PC>
2016-07-31 22:32:47 +0900

    add sql

---
+++ web/WEB-INF/sql/shop.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE public.shop
+(
+  shop_id integer NOT NULL DEFAULT nextval('shop_info_shop_id_seq'::regclass),
+  shop_name character varying(50),
+  shop_prefecture character varying(6),
+  shop_city character varying(100),
+  building_name character varying(50),
+  nearest_station character varying(50),
+  shop_phone_number character varying(12),
+  shop_url character varying(2083)[],
+  recommand_count integer,
+  CONSTRAINT shop_info_pkey PRIMARY KEY (shop_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.shop
